a417f88bc5a42cc99375763063ada25c76d09f6b
hala-madrid2414 <1006631136@qq.com>
2025-11-30 16:27:56 +0800

    feat: 初始化项目基础结构和资源文件
    添加项目基础文件结构，包括配置文件、资源图片和初始代码

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useCallback, useEffect, useState } from '@lynx-js/react'
+
+import './App.css'
+import { PromotionSection } from './PromotionSection.jsx'
+
+export function App() {
+  return (
+    <view>
+      <view className='App'>
+        <PromotionSection />
+        <text>Hello World!</text>
+      </view>
+    </view>
+  )
+}
